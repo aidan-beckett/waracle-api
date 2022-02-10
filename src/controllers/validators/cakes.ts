606bc00de1823d560877a364328f9da492136c12
Aidan Beckett <aidan.beckett@synextra.co.uk>
2022-02-10 15:38:30 +0000

    Update cakes.ts

--- src/controllers/validators/cakes.ts
+++ src/controllers/validators/cakes.ts
@@ -9,9 +9,11 @@ export module CakeValidator {
 
     if(!body.name) errorMessages.push("A Cake must have a Name");
     else if(!isString(body.name)) errorMessages.push("Name must be a string");
+    else if(body.name.length > 30) errorMessages.push("Name must be 30 characters or less");
 
     if(!body.comment) errorMessages.push("A Cake must have a Comment");
     else if(!isString(body.comment)) errorMessages.push("Comment must be a string");
+    else if(body.comment.length > 200) errorMessages.push("Comment must be 200 characters or less");
 
     if(!body.imageUrl) errorMessages.push("A Cake must have an Image Url");
     else if(!isString(body.imageUrl)) errorMessages.push("Image Url must be a string");
@@ -27,7 +29,11 @@ export module CakeValidator {
     const errorMessages:string[] = [];
 
     if(body.name && !isString(body.name)) errorMessages.push("Name must be a string");
+    else if(body.name && body.name.length > 30) errorMessages.push("Name must be 30 characters or less");
+
     if(body.comment && !isString(body.comment)) errorMessages.push("Comment must be a string");
+    else if(body.comment && body.comment.length > 200) errorMessages.push("Comment must be 200 characters or less");
+
     if(body.imageUrl && !isString(body.imageUrl)) errorMessages.push("Image Url must be a string");
 
     if(body.yumFactor && !isNumber(body.yumFactor)) errorMessages.push("Yum Factor must be a number");
